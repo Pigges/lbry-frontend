db8759add7e35656715e96a339f8eede643ef5b5
infinite-persistence <inf.persistence@gmail.com>
2022-01-04 20:07:48 +0800

    Keycloak - put back original code that makes `user=null` mean `failed fetch`

--- ui/redux/reducers/user.js
+++ ui/redux/reducers/user.js
@@ -25,7 +25,7 @@ const defaultState: UserState = {
   invitees: undefined,
   referralLink: undefined,
   referralCode: undefined,
-  user: { has_verified_email: false },
+  user: undefined,
   accessToken: undefined,
   youtubeChannelImportPending: false,
   youtubeChannelImportErrorMessage: '',
@@ -36,7 +36,6 @@ const defaultState: UserState = {
   homepageFetched: false,
 };
 
-// does this do anything with user?
 reducers[ACTIONS.AUTHENTICATION_STARTED] = (state) =>
   Object.assign({}, state, {
     authenticationIsPending: true,
@@ -65,6 +64,7 @@ reducers[ACTIONS.AUTHENTICATION_FAILURE] = (state) =>
   Object.assign({}, state, {
     authenticationIsPending: false,
     userIsPending: false,
+    user: null,
   });
 
 reducers[ACTIONS.USER_FETCH_STARTED] = (state) =>
