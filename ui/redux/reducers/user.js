27812d7c261672cd4313584f3b46234d227c29b4
infinite-persistence <inf.persistence@gmail.com>
2022-01-21 10:32:23 +0800

    Remove the need to save both tokens into Redux
    It was probably more of a Desktop thing. Probably safer to grab from cookies store and keycloak plugin.

--- ui/redux/reducers/user.js
+++ ui/redux/reducers/user.js
@@ -26,7 +26,6 @@ const defaultState: UserState = {
   referralLink: undefined,
   referralCode: undefined,
   user: undefined,
-  accessToken: undefined,
   youtubeChannelImportPending: false,
   youtubeChannelImportErrorMessage: '',
   referrerSetIsPending: false,
@@ -51,9 +50,6 @@ reducers[ACTIONS.AUTHENTICATION_SUCCESS] = (state, action) => {
   if (action.data) {
     if (action.data.user) {
       newUserState.user = action.data.user;
-    }
-    if (action.data.accessToken) {
-      newUserState.accessToken = action.data.accessToken;
     }
   }
 
