iamflorencejay/odysee-frontend

Commentron: add 'channel.Status'

flow-typed/Comment.js
@@ -21,6 +21,13 @@ declare type Comment = {
   removed?: boolean,
 };
 
+declare type Authorization = {
+  channel_name: string,
+  channel_id: string,
+  signature?: string,
+  signature_ts?: string,
+};
+
 declare type PerChannelSettings = {
   words?: Array<string>,
   comments_enabled?: boolean,
@@ -303,4 +310,13 @@ declare type BlockWordParams = {
   signature: string,
   signing_ts: string,
   words: string, // CSV list of containing words to block comment on content
+};
+
+declare type ChannelStatusParams = {
+  MyChannels: Array<Authorization>,
+};
+
+declare type ChannelStatusResponse = {
+  Confirmed: Array<Authorization>,
+  UnConfirmed: Array<Authorization>,
 };

Pigges/lbry-frontend

flow-typed/Comment.js
@@ -87,8 +87,8 @@ declare type CommentsState = {
 declare type Authorization = {
   channel_name: string,
   channel_id: string,
-  signature: string,
-  signing_ts: string,
+  signature?: string,
+  signing_ts?: string,
 };
 
 // ModAuthorization parameters for calls requiring creator/moderator authentication
@@ -328,4 +328,13 @@ declare type BlockWordParams = {
   signature: string,
   signing_ts: string,
   words: string, // CSV list of containing words to block comment on content
+};
+
+declare type ChannelStatusParams = {
+  MyChannels: Array<Authorization>,
+};
+
+declare type ChannelStatusResponse = {
+  Confirmed: Array<Authorization>,
+  UnConfirmed: Array<Authorization>,
 };